7424818ce5acf6682a2e931facb23d4bb9dbe8c6
Hongli Lai (Phusion) <hongli@phusion.nl>
2010-07-25 21:27:35 +0200

    Begin changing license.

---
+++ misc/change_license.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+FILES = Dir[
+	"bin/*",
+	"ext/{apache2,common,nginx,phusion_passenger}/**/*.{c,cpp,h,hpp}",
+	"helper-scripts/*",
+	"lib/**/*.rb",
+	"lib/**/*.py",
+]
+LICENSE_START_SIGNATURE = '"Phusion Passenger" is a trademark of Hongli Lai & Ninh Bui.'
+NEW_LICENSE = "See LICENSE file for license information."
+
+def change_license!(filename)
+	result = false
+	contents = File.read(filename)
+	if contents =~ /\A((#.*?\n)+)/m
+		header  = $1
+		license_start = contents.index(LICENSE_START_SIGNATURE)
+		if license_start
+			license_start += LICENSE_START_SIGNATURE.size
+			license_end = header.size
+			contents[license_start .. license_end - 1] = "\n#\n#  #{NEW_LICENSE}\n"
+			result = true
+		end
+	elsif contents =~ %r(\A(/\*.+?\*/))m
+		header = $1
+		license_start = contents.index(LICENSE_START_SIGNATURE)
+		if license_start
+			license_start += LICENSE_START_SIGNATURE.size
+			license_end = header.size
+			contents[license_start .. license_end - 1] = "\n *\n *  #{NEW_LICENSE}\n */"
+			result = true
+		end
+	end
+	File.open(filename, 'w') do |f|
+		f.write(contents)
+	end
+	return result
+rescue EOFError
+end
+
+FILES.each do |filename|
+	printf("%-65s", filename)
+	STDOUT.flush
+	if change_license!(filename)
+		puts "  ok!"
+	else
+		puts "  skipped"
+	end
+end
