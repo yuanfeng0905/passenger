69f5029fd4c34654cad90ad0856ccebc584f5363
Hongli Lai (Phusion) <hongli@phusion.nl>
2013-02-28 19:07:42 +0100

    Show Flying Passenger in passenger-memory-stats

--- lib/phusion_passenger/admin_tools/memory_stats.rb
+++ lib/phusion_passenger/admin_tools/memory_stats.rb
@@ -71,7 +71,7 @@ class MemoryStats
 	# Phusion Passenger is not running.
 	def passenger_processes
 		@passenger_processes ||= list_processes(:match =>
-			/((^| )Passenger |(^| )Rails:|(^| )Rack:|PassengerHelperAgent|PassengerWatchdog|PassengerLoggingAgent|wsgi-loader.py)/)
+			/((^| )Passenger |(^| )Rails:|(^| )Rack:|PassengerHelperAgent|PassengerWatchdog|PassengerLoggingAgent|wsgi-loader.py|flying-passenger)/)
 	end
 	
 	# Returns the sum of the memory usages of all given processes.
