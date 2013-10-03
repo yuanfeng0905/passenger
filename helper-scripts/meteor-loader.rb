4ae497ee75d68006124693580ae82a0054e7397d
Hongli Lai (Phusion) <hongli@phusion.nl>
2013-10-03 15:30:37 +0200

    Change license

--- helper-scripts/meteor-loader.rb
+++ helper-scripts/meteor-loader.rb
@@ -10,6 +10,9 @@
 #  See LICENSE file for license information.
 
 #
+#  See LICENSE file for license information.
+
+#
 #  Permission is hereby granted, free of charge, to any person obtaining a copy
 #  of this software and associated documentation files (the "Software"), to deal
 #  in the Software without restriction, including without limitation the rights
