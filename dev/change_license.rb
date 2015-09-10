e64a107fc443e1749b215d60a19e1cfb32a1dc1f
Hongli Lai (Phusion) <hongli@phusion.nl>
2015-09-10 21:01:37 +0800

    Update license headers

--- dev/change_license.rb
+++ dev/change_license.rb
@@ -1,51 +1,53 @@
 #!/usr/bin/env ruby
+# encoding: utf-8
+
 FILES = Dir[
-	"bin/*",
-	"build/*.rb",
-	"ext/{apache2,common,nginx,ruby}/**/*.{c,cpp,h,hpp,erb}",
-	"helper-scripts/**/*",
-	"lib/**/*.rb",
-	"lib/**/*.py",
-]
+  "bin/*",
+  "build/*.rb",
+  "src/**/*.{c,cpp,h,hpp,erb,rb,py,js}",
+  "src/helper-scripts/**/*"
+] - Dir["src/cxx_supportlib/vendor*/**/*"]
 LICENSE_START_SIGNATURE = '"Phusion Passenger" is a trademark of Hongli Lai & Ninh Bui.'
 NEW_LICENSE = "See LICENSE file for license information."
 
 def change_license!(filename)
-	result = false
-	contents = File.read(filename)
-	if contents =~ /\A((#.*?\n)+)/m
-		header  = $1
-		license_start = contents.index(LICENSE_START_SIGNATURE)
-		if license_start
-			license_start += LICENSE_START_SIGNATURE.size
-			license_end = header.size
-			contents[license_start .. license_end - 1] = "\n#\n#  #{NEW_LICENSE}\n"
-			result = true
-		end
-	elsif contents =~ %r(\A(/\*.+?\*/))m
-		header = $1
-		license_start = contents.index(LICENSE_START_SIGNATURE)
-		if license_start
-			license_start += LICENSE_START_SIGNATURE.size
-			license_end = header.size
-			contents[license_start .. license_end - 1] = "\n *\n *  #{NEW_LICENSE}\n */"
-			result = true
-		end
-	end
-	File.open(filename, 'w') do |f|
-		f.write(contents)
-	end
-	return result
+  result = false
+  contents = File.open(filename, 'r:utf-8') do |f|
+    f.read
+  end
+  if contents =~ /\A((#.*?\n)+)/m
+    header  = $1
+    license_start = contents.index(LICENSE_START_SIGNATURE)
+    if license_start
+      license_start += LICENSE_START_SIGNATURE.size
+      license_end = header.size
+      contents[license_start .. license_end - 1] = "\n#\n#  #{NEW_LICENSE}\n"
+      result = true
+    end
+  elsif contents =~ %r(\A(/\*.+?\*/))m
+    header = $1
+    license_start = contents.index(LICENSE_START_SIGNATURE)
+    if license_start
+      license_start += LICENSE_START_SIGNATURE.size
+      license_end = header.size
+      contents[license_start .. license_end - 1] = "\n *\n *  #{NEW_LICENSE}\n */"
+      result = true
+    end
+  end
+  File.open(filename, 'w:utf-8') do |f|
+    f.write(contents)
+  end
+  return result
 rescue EOFError
 end
 
 FILES.each do |filename|
-	next if File.directory?(filename)
-	printf("%-65s", filename)
-	STDOUT.flush
-	if change_license!(filename)
-		puts "  ok!"
-	else
-		puts "  skipped"
-	end
+  next if File.directory?(filename)
+  printf("%-65s", filename)
+  STDOUT.flush
+  if change_license!(filename)
+    puts "  ok!"
+  else
+    puts "  skipped"
+  end
 end
