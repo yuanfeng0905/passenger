16d923ac8137af7001d7790ed30cfdc326707c54
Hongli Lai (Phusion) <hongli@phusion.nl>
2016-10-12 15:21:52 +0200

    Rename support@phusion.nl to support@phusionpassenger.com

--- src/cxx_supportlib/Utils/License.h
+++ src/cxx_supportlib/Utils/License.h
@@ -20,7 +20,7 @@ namespace Passenger {
 	"The license key must be an exact byte-by-byte copy of the one downloaded from the Customer Area. " \
 	"It is already considered corrupted if you add a newline to it, which can happen if you e.g. copy-paste the contents using a text editor. " \
 	"Please use a binary copying tool to copy the license key to your server, e.g. 'scp' or FTP in binary mode. " \
-	"If the problem persists, please contact support@phusion.nl.\n" \
+	"If the problem persists, please contact support@phusionpassenger.com.\n" \
 	"If you do not have a valid license, then we kindly ask you not to try to crack this software or to obtain it illegally. " \
 	"We're trying to make a living out of this and the profits are used to fund the development of the " \
 	"open source version of " PROGRAM_NAME ". We can only do this with your support. If you don't have " \
